5e1adf6d02163af28f7b2e7aff3333e60a90a2a4
pinniakhil <akhil.pinni@neosoftmail.com>
2022-03-01 11:48:52 +0530

    deploy my-app usestate hook example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [name, setName] = useState('')
+  const prevName = useRef('')
+
+  useEffect(() => {
+     prevName.current =  name
+  }, [name])
+
+  return(
+    <>
+    <input  value={name} onChange={e => setName(e.target.value)}/>
+    <div>My name is {name} and it is used to be {prevName.current}</div>
+    
+    </>
+  ) 
 }
 
 export default App;
